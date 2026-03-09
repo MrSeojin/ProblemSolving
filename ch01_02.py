1f9d1d78b90654396aaa5ae4ad85d81e47503920
Seojin.K <7701seojin@gmail.com>
2026-03-10 03:41:36 +0900

    ch01_02 ACM 호텔 (백준)

---
+++ ch01_02.py
@@ -0,0 +1,14 @@
+# ACM Hotel
+
+T = int(input())
+
+room = []
+for _ in range(T):
+    H, W, N = map(int, input().split())
+    if (N % H == 0):
+        room.append( (H * 100) + (N//H))
+    else:
+        room.append( (N % H)*100 + (N//H) + 1 )
+
+for a in range (T):
+    print(room[a])
