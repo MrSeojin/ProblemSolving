17950ed96f18ccff16d9d0d9a7bc75ed3d57ef3a
Seojin.K <7701seojin@gmail.com>
2026-03-10 02:47:58 +0900

    ch01_01 하얀칸 (백준)

---
+++ ch01_01.py
@@ -0,0 +1,11 @@
+# White square
+
+count = 0   # 하얀칸 위에 말의 개수
+
+for j in range(8):
+    L = input()
+    for i in range(8):
+        if (i + j) % 2 == 0 and L[i] =='F':
+            count += 1
+
+print(count)
